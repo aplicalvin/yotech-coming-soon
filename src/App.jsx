524a5a7dc68aacfa5f158931ef62b025232fb2c9
aplicalvin <thecalvins.sims@gmail.com>
2024-06-26 15:55:32 +0700

    fixed ui

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,13 @@ function App() {
 
     return (
         <>
-            <div className="w-full h-full min-h-screen dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-                {/* Radial gradient for the container to give a faded look */}
-                {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div> */}
-                <div>
+            <div className="w-full h-full min-h-screen bg-lime-950 bg-dot-white/[0.2] relative flex items-center justify-center">
+                <div className="mx-4 grid gap-0">
                     <img src={Logo} alt="" className="rounded-full w-48" />
-                    <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+                    <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-4">
                         Yolo Technology <br /> Semarang <br />
                     </p>
-                    <p className="text-2xl text-white">is Coming Soon</p>
+                    <p className="text-2xl text-white/60">is Coming Soon</p>
                 </div>
             </div>
         </>
